7e34b654f7e145ba2f66ba9715ab89f8c1d93cbd
vitobione <56788392+vitobione@users.noreply.github.com>
2023-06-15 13:01:14 +0200

    Update mergehere.py
    error handling
    progress bar

--- mergehere.py
+++ mergehere.py
@@ -1,38 +1,34 @@
 import os
 import pandas as pd
+from pathlib import Path
+from tqdm import tqdm
 
 def merge_csvs(folder_path, output_file):
-    # Use pathlib module for working with file paths
-    from pathlib import Path
-    
-    # Use glob to find CSV files
-    csv_files = Path(folder_path).glob('*.csv')
-    csv_files = list(csv_files)  # Convert generator to a list
-    
-    if not csv_files:
-        print("No CSVs in this folder 🤨")
-        return
-    
-    dfs = []
-    common_columns = set()  # Use set for faster lookup
-    
-    for csv_file in csv_files:
-        df = pd.read_csv(csv_file)
+    try:
+        # Find CSV files using glob
+        csv_files = list(Path(folder_path).glob('*.csv'))
         
-        # Update common_columns using set operations
-        common_columns.update(df.columns)
+        if not csv_files:
+            print("No CSVs in this folder 🤨")
+            return
         
-        # Reindex the DataFrame with common columns
-        df = df.reindex(columns=common_columns)
+        dfs = []
+        common_columns = set()
         
-        # Add 'Source' column with file name
-        df['Source'] = csv_file.name
+        # Use tqdm to show progress
+        for csv_file in tqdm(csv_files, desc="Merging CSVs", unit="file"):
+            df = pd.read_csv(csv_file)
+            common_columns.update(df.columns)
+            df = df.reindex(columns=common_columns)
+            df['Source'] = csv_file.name
+            dfs.append(df)
         
-        dfs.append(df)
+        merged_df = pd.concat(dfs, ignore_index=True)
+        merged_df.to_csv(output_file, index=False)
+        print(f"CSVs have been merged and saved to '{output_file}' 🤗")
     
-    merged_df = pd.concat(dfs, ignore_index=True)
-    merged_df.to_csv(output_file, index=False)
-    print(f"CSVs have been merged and saved to '{output_file}' 🤗")
+    except (FileNotFoundError, PermissionError) as e:
+        print(f"Error: {e}")
 
 # Get the location where the script is
 script_dir = os.getcwd()
